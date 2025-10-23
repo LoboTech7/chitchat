2ff08bbf958a7b01b79958dfd7dc9fd823542d17
nagasawalin <shiezhenlin@gmail.com>
2025-10-23 12:41:16 +0200

    added leave function

--- server/server.go
+++ server/server.go
@@ -13,6 +13,7 @@ import (
 type chitchatService struct {
 	proto.UnimplementedChitchatServer
 	time_stamp []int32 //slice of a timestamps
+	next_user  int32
 	users      []int32 // amount of users
 	messages   []string
 }
@@ -20,6 +21,7 @@ type chitchatService struct {
 func main() {
 	server := &chitchatService{
 		time_stamp: []int32{},
+		next_user:  0,
 		users:      []int32{},
 		messages:   []string{},
 	} //creates server -> timestamp/users is a empty slice of int32
@@ -44,6 +46,27 @@ func (s *chitchatService) start_server() {
 
 func (s *chitchatService) Join(ctx context.Context, in *proto.Empty) (*proto.User, error) {
 	s.time_stamp = append(s.time_stamp, 0)
-	s.users = append(s.users, int32(len(s.users)))
+	s.users = append(s.users, int32(s.next_user))
+	s.next_user++
 	return &proto.User{TimeStamp: s.time_stamp, UserId: s.users[len(s.users)-1]}, nil
+}
+
+func (s *chitchatService) Leave(ctx context.Context, in *proto.User) (*proto.Empty, error) {
+	// handle client's leave request and return emprty response
+	for i, val := range s.time_stamp {
+		if val < in.TimeStamp[i] {
+			s.time_stamp[i] = in.TimeStamp[i] //checkin the timestamp stuff
+		}
+	}
+
+	var index int
+	for i, val := range s.users {
+		if val == in.UserId {
+			index = i
+			break
+		}
+	}
+
+	s.users = append(s.users[:index], s.users[index+1:]...) //
+	return &proto.Empty{}, nil
 }
