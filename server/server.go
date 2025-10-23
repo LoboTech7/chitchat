7f185c615454e334b97b46e09ce183d5db757e5c
Lobo <dsab@itu.dk>
2025-10-23 11:14:10 +0200

    server

--- server/server.go
+++ server/server.go
@@ -2,12 +2,14 @@ package main
 
 import proto "chitchat/grpc"
 
+// this is the info the server remembers
 type chitchatService struct {
 	proto.UnimplementedChitchatServer
-	time_stamp []int32 //slice of a timestamp!?
-	user_id    int32
+	time_stamp []int32 //slice of a timestamps
+	users      []int32 // amount of users
 }
 
 func main() {
+	server := &chitchatService{time_stamp: []int32{}, users: []int32{}} //creates server -> timestamp/users is a empty slice of int32
 
 }
